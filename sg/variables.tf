764d9042150e36d08ff4f370c595c3c601312f25
Peri Adanova <adanova.p@gmail.com>
2024-02-23 18:39:47 -0800

    changes sg

--- sg/variables.tf
+++ sg/variables.tf
@@ -18,30 +18,3 @@ variable "sg_rules" {
 
 }
 
-variable "sg_description" {
-
-}
-
-variable "type" {
-
-}
-
-variable "from_port" {
-
-}
-
-variable "to_port" {
-
-}
-
-variable "protocol" {
-
-}
-
-variable "cidr_blocks" {
-
-}
-
-variable "source_security_group_id" {
-
-}
