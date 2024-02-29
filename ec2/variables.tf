4616107db723fcee5640e79a412aea41e67b0186
Peri Adanova <adanova.p@gmail.com>
2024-02-28 16:05:06 -0800

    update ec2

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -4,25 +4,24 @@ variable "ami" {
 
 variable "instance_type" {
   type = string
-
 }
 
 variable "key_name" {
-
+  type = string
 }
 
 variable "vpc_security_group_ids" {
-
+  type = list(string)
 }
 
 variable "subnet_id" {
-
+  type = string
 }
 
 variable "user_data" {
-
+  type = string
 }
 
-variable "ins_tag" {
-
+variable "instance_tag" {
+  type = string
 }
