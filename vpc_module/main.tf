6febbed61d20ca06d286b0dc2acfd2465099637c
Peri <78322293+PeriAdan@users.noreply.github.com>
2024-02-29 17:47:27 -0800

    Update main.tf

--- vpc_module/main.tf
+++ vpc_module/main.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "vpc_module" {
     Name = var.custom_tag
   }
 }
+
+
+resource "aws_internet_gateway" "igw" {
+  count = var.create_attach_igw ? 1 : 0
+
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.vpc_tag}_igw"
+  }
+}
